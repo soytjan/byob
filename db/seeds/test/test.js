c5bbf43091acc9787b1213249ba24bdc8d61e876
Amanda Tjan <amanda.tjan@gmail.com>
2018-03-31 12:22:47 -0600

    Set up test seeds

---
+++ db/seeds/test/test.js
@@ -0,0 +1,45 @@
+
+const characterData = require('../../../mockData/character-data.js');
+const familyData = require('../../../mockData/familyNames.js');
+
+const createFamily = (knex, familyName) => {
+  return knex('families').insert({
+    name: familyName
+  }).returning(['id', 'name'])
+};
+
+const createCharacter = (knex, character, familyId) => {
+  const updatedChar = {
+    name: character.name,
+    book_presence: character.book_presence,
+    description: character.description,
+    family_id: familyId
+  };
+  return knex('characters').insert(updatedChar, 'id'); 
+};
+
+exports.seed = function(knex, Promise) {
+  return knex('characters').del()
+    .then(() => knex('families').del())
+    .then(() => {
+      let familyPromises = [];
+
+      familyData.forEach(family => {
+        familyPromises.push(createFamily(knex, family))
+      })
+
+      return Promise.all(familyPromises);
+    })
+    .then((familyNames) => {
+      let characterPromises = [];
+
+      for(let i = 0; i < characterData.length; i++) {
+        const { family_name } = characterData[i];
+        const familyFound = familyNames.find(familyName => familyName[0].name === family_name)
+        characterPromises.push(createCharacter(knex, characterData[i], familyFound[0].id))
+      }
+      return Promise.all(characterPromises); 
+
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`))
+};
---
+++ test/routes.spec.js
@@ -0,0 +1,70 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should return the homepage with text', () => {
+    return chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.html;
+      })
+      .catch(err => {
+        throw err;
+    });
+  });
+
+  it('should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/sad')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(err => {
+        throw err;
+    });
+  });
+});
+
+describe('API Routes', () => {
+  beforeEach((done) => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
+        .then(() => {
+          done()
+        })
+      })
+    })
+  })
+
+  describe('GET /api/v1/families', () => {
+    it('should return all of the families', () => {
+      return chai.request(server)
+        .get('/api/v1/families')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(29);
+          response.body[0].should.have.property('name');
+          response.body[0].name.should.equal('Abbott');
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
+        })
+        .catch(error => {
+          throw error;
+      })
+    })
+  })
+})
