88f8a7d039e9a0e5817e880134a77c215e6dcbf1
Amanda Tjan <amanda.tjan@gmail.com>
2018-03-31 11:36:34 -0600

    Add in test and production environment to knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,24 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palette_picker_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
